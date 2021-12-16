be1400ff349a1f7e60a69aa319e82f4e3132aab4
StrawaDominik <92323963+StrawaDominik@users.noreply.github.com>
2021-12-16 14:19:47 +0100

    enter

---
+++ script.js
@@ -0,0 +1,9 @@
+"use strict"
+
+let witacz = document.getElementById("witacz")
+
+let odwiedzajacy = prompt("Wpisz swoje imię");
+  if (odwiedzajacy !== null) {
+    witacz.innerHTML = "Witaj na stronie " + odwiedzajacy + "!";
+    
+  }
